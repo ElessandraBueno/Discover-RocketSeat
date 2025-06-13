6b6f85b306f2ca39de9262f1a9285f817db8fdfc
Elessandra Bueno <72478092+ElessandraBueno@users.noreply.github.com>
2025-06-13 20:36:09 -0300

    Concluindo o site

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 function toggleMode(){
     const html = document.documentElement
-
+    html.classList.toggle('light')
+    
     // if(html.classList.contains('light')){
     //     html.classList.remove('light')
     // }else{
     //     html.classList.add('light')
     // }
-    html.classList.toggle('light')
+
+    const img = document.querySelector('#profile img')
+    if(html.classList.contains('light')){
+        img.setAttribute('src', 'assets/avatar-light.png')
+        img.setAttribute('alt', 'Foto de Mayk Brito de oculos escuro com fundo azul')
+    }else{
+        img.setAttribute('src', 'assets/avatar.png')
+        img.setAttribute('alt', 'Foto de Mayk Brito sorrindo, usando oculos e camisa preta, barba e fundo amarelo')
+    }
 }
