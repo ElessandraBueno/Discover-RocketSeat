5370c4910ea6cf4cea896e65d822702f533b117a
Elessandra Bueno <72478092+ElessandraBueno@users.noreply.github.com>
2025-07-13 13:43:02 -0300

    Atualizando informações

--- script.js
+++ script.js
@@ -11,9 +11,9 @@ function toggleMode(){
     const img = document.querySelector('#profile img')
     if(html.classList.contains('light')){
         img.setAttribute('src', 'assets/avatar-light.png')
-        img.setAttribute('alt', 'Foto de Mayk Brito de oculos escuro com fundo azul')
+        img.setAttribute('alt', 'Foto de Elessandra Bueno com blusa preta e fundo rosado')
     }else{
         img.setAttribute('src', 'assets/avatar.png')
-        img.setAttribute('alt', 'Foto de Mayk Brito sorrindo, usando oculos e camisa preta, barba e fundo amarelo')
+        img.setAttribute('alt', 'Foto de Elessandra Bueno com blusa preta e fundo rosado')
     }
 }
